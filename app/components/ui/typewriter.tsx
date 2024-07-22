db770f27ae02d887ba03157db5e5ac41e04a6267
Jackie Vo <71198906+jacklvd@users.noreply.github.com>
2024-07-22 16:11:56 +0000

    feat: update landing page to responsive

--- app/components/ui/typewriter.tsx
+++ app/components/ui/typewriter.tsx
@@ -4,7 +4,7 @@ import Typewriter from 'typewriter-effect'
 
 export const Writer: React.FC = () => {
     return (
-        <div className='lg:text-5xl md:text-3xl sm:text-md text-gray-900'>
+        <div className='lg:text-5xl sm:text-4xl sm:text-md text-gray-900'>
             <Typewriter
                 options={{
                     loop: true,
@@ -22,6 +22,10 @@ export const Writer: React.FC = () => {
                         .deleteAll()
                         .typeString('a Bun Bo Hue Lover')
                         .pauseFor(1000)
+                        .deleteAll()
+                        .typeString('a Traveler')
+                        .pauseFor(1000)
+                        .deleteAll()
                         .start()
                 }}
             />
