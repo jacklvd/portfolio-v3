f66b523b4bd5d8b0016f6b2787ba122963275d5e
jacklvd <vodanglong3103@gmail.com>
2024-08-04 18:25:44 -0400

    feat: fix footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <>
       <div className="text-center pb-18">
         <div>Built and Designed by Jack Vo</div>
-        <div>All rights reserved &amp;copy; {new Date().getFullYear()}</div>
+        <div>All rights reserved &copy; {new Date().getFullYear()}</div>
       </div>
     </>
   )
