6f30285e52101e72ac1946f9dd18cdb4712a45cd
jacklvd <vodanglong3103@gmail.com>
2025-03-26 16:31:38 -0400

    remove unused page

--- app/albums/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Polaroids } from '../components/polaroids'
-
-export default function Portfolio() {
-  return (
-    <div className="flex flex-col items-center justify-between p-16 sm:p-20 md:p-24">
-      <Polaroids />
-    </div>
-  )
-}
